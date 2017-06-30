9d637d77d497c73356138e2bc9ad3590c2d11887
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-06-30 16:55:16 -0300

    Pass json data to LibraryReducer and test App

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './src/reducers';
+import { Header } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Header textHeader="Header" />
         </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
