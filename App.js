98268b2838a39f676ad6589354cbf196cda7a415
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-01 16:29:42 -0300

    Create LibraryList component and connect it with redux store

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
           <Header textHeader="Header" />
+          <LibraryList />
         </View>
       </Provider>
     );
